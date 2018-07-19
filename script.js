8efd2a880ef53636981a490ff2f15034a5a5be48
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-19 23:35:37 +0300

    four tests passed

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // // document.addEventListener('DOMContentLoaded',function(){
 
-
-
-
-const loadData = async () => {
-    req = await axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+const loadData = async() => {
+    req = await axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-d' +
+            'ata.json');
     const dataset = req.data.data;
 
     const margin = {
@@ -13,14 +11,11 @@ const loadData = async () => {
         bottom: 10,
         left: 20
     };
-    const
-        svgWidth = 900 + margin.left + margin.right,
+    const svgWidth = 900 + margin.left + margin.right,
         svgHeight = 500,
         barPadding = 0.1;
     const barwidth = svgWidth / dataset.length;
-    // not: barwidth has to be greater than barPadding;
-
-    // Create an x and y scale
+    // not: barwidth has to be greater than barPadding; Create an x and y scale
 
     const years = dataset.map(d => d[0]);
     const gdpData = dataset.map(d => d[1]);
@@ -30,47 +25,71 @@ const loadData = async () => {
 
     // document.getElementById("demo").innerHTML=gdpMax;
 
-    const xScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, (d) => d[0])])
-        .range([barPadding, svgWidth - barPadding]);
+    const xScale = d3
+        .scaleLinear()
+        .domain([
+            0, d3.max(dataset, (d) => d[0])
+        ])
+        .range([
+            barPadding, svgWidth - barPadding
+        ]);
 
     // Add your code below this line
 
-    const yScale = d3.scaleLinear().domain([0, gdpMax]).range([0, svgHeight - margin.top - margin.bottom]);
-    const svg = d3.select("body")
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, gdpMax])
+        .range([
+            0, svgHeight - margin.top - margin.bottom
+        ]);
+    const svg = d3
+        .select("body")
         .append("svg")
-        .attr("width", svgWidth )
+        .attr("width", svgWidth)
         .attr("height", svgHeight)
         .style("background", "pink")
 
-
-    const barChart = svg.selectAll("rect")
+    const barChart = svg
+        .selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
-        .attr("x", (d, i) => i * barwidth+ margin.left)
-        .attr("y", (d, i) => svgHeight  - margin.bottom - yScale(d[1]))
+        .attr("x", (d, i) => i * barwidth + margin.left)
+        .attr("y", (d, i) => svgHeight - margin.bottom - yScale(d[1]))
         .attr("width", barwidth - barPadding)
         .attr("height", (d) => yScale(d[1]))
         .attr("fill", "grey")
 
-
-    yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([svgHeight, 0]);
-    xAxisScale = d3.scaleLinear()
-        .domain([d3.min(years), d3.max(years)])
+    yAxisScale = d3
+        .scaleLinear()
+        .domain([0, gdpMax])
+        .range([svgHeight, 0]);
+    xAxisScale = d3
+        .scaleLinear()
+        .domain([
+            d3.min(years),
+            d3.max(years)
+        ])
         .range([0, svgWidth]);
 
-    xAxis = d3.axisBottom().scale(xAxisScale);
-    yAxis = d3.axisLeft().scale(yAxisScale);
+    xAxis = d3
+        .axisBottom()
+        .scale(xAxisScale);
+    yAxis = d3
+        .axisLeft()
+        .scale(yAxisScale);
 
-    svg.append("g")
+    svg
+        .append("g")
         .call(yAxis)
         .attr("id", "y-axis")
         .attr("transform", "translate(20, 10)")
 
     xAxisTranslate = svgHeight - 10;
 
-    svg.append("g")
+    svg
+        .append("g")
+        .attr("id", "x-axis")
         .attr("transform", "translate(50, " + xAxisTranslate + ")")
         .call(xAxis)
 
@@ -80,47 +99,16 @@ const loadData = async () => {
 
 loadData()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ALternative to setting the x and y positions of the bars
-// const barChart= svg.selectAll("rect")
-// .data(dataset)
-// .enter()
-// .append("rect")
-// // .attr("x", (d, i) => i * barwidth)
-// // .attr("y", (d, i) => svgHeight - yScale(d[1]))
-// .attr("transform", (d,i)=>{
-//     let x=  i * barwidth;
-//     let y = svgHeight - yScale(d[1]);
-//     return `translate(${x}, ${y})`;
-// })
-// .attr("width", barwidth - barPadding)
-// .attr("height", (d) => yScale(d[1]))
-// .attr("fill", "grey")
-
-
-
-
-// function loadData() {
-//     let req = new XMLHttpRequest();
-//     req.onload = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             let json = JSON.parse(this.responseText);
-//             // document.getElementById("demo").innerHTML = req.responseText;
-//             console.log(json.data)
-//         }
-//     };
-//     req.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", true);
-//     req.send();
-// }
+// ALternative to setting the x and y positions of the bars const barChart=
+// svg.selectAll("rect") .data(dataset) .enter() .append("rect") // .attr("x",
+// (d, i) => i * barwidth) // .attr("y", (d, i) => svgHeight - yScale(d[1]))
+// .attr("transform", (d,i)=>{     let x=  i * barwidth;     let y = svgHeight -
+// yScale(d[1]);     return `translate(${x}, ${y})`; }) .attr("width", barwidth
+// - barPadding) .attr("height", (d) => yScale(d[1])) .attr("fill", "grey")
+// function loadData() {     let req = new XMLHttpRequest();     req.onload =
+// function () {         if (this.readyState == 4 && this.status == 200) {
+//       let json = JSON.parse(this.responseText);             //
+// document.getElementById("demo").innerHTML = req.responseText;
+// console.log(json.data)         }     };     req.open("GET",
+// "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GD
+// P-data.json", true);     req.send(); }
