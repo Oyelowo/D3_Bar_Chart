26a24cea287fa5782b568c46e89759d6f5d1fa6b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-20 21:57:31 +0300

    margins considered in width and height values for chart areas

--- script.js
+++ script.js
@@ -14,35 +14,28 @@ const loadData = async() => {
     const svgWidth = 900,
         svgHeight = 500,
         barPadding = 0;
-    const barwidth = (svgWidth - margin.right - margin.left)/ dataset.length;
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+    const barwidth = (width) / dataset.length;
+
     // note: barwidth has to be greater than barPadding; Create an x and y scale
 
     const gpdDates = dataset.map(d => d[0]);
-    const gdpYears = gpdDates.map(d=>d.slice(0,4));
+    const gdpYears = gpdDates.map(d => d.slice(0, 4));
     const gdpData = dataset.map(d => d[1]);
 
     const gdpMin = d3.min(gdpData);
     const gdpMax = d3.max(gdpData);
 
-    // document.getElementById("demo").innerHTML=gdpMax;
-
-    // const xScale = d3
-    //     .scaleLinear()
-    //     .domain([
-    //         0, d3.max(dataset, (d) => d[0])
-    //     ])
-    //     .range([
-    //         barPadding, svgWidth - barPadding - margin.left-margin.right
-    //     ]);
-
-    // Add your code below this line
+    // document.getElementById("demo").innerHTML=gdpMax; const xScale = d3
+    // .scaleLinear()     .domain([         0, d3.max(dataset, (d) => d[0])     ])
+    // .range([         barPadding, svgWidth - barPadding - margin.left-margin.right
+    //     ]); Add your code below this line
 
     const yScale = d3
         .scaleLinear()
         .domain([0, gdpMax])
-        .range([
-            0, svgHeight - margin.top - margin.bottom
-        ]);
+        .range([0, height]);
     const svg = d3
         .select("body")
         .append("svg")
@@ -56,26 +49,28 @@ const loadData = async() => {
         .enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("data-date", (d)=> d[0])
-        .attr("data-gdp", (d)=> d[1])
+        .attr("data-date", (d) => d[0])
+        .attr("data-gdp", (d) => d[1])
         .attr("x", (d, i) => i * barwidth)
         .attr("y", (d, i) => svgHeight - yScale(d[1]))
         .attr("width", barwidth - barPadding)
         .attr("height", (d) => yScale(d[1]))
         .attr("fill", "lightgreen")
-        .attr("transform", `translate(${margin.left},${-margin.bottom})`)
+        .attr("transform", `translate(${margin.left},${ - margin.bottom})`)
 
     yAxisScale = d3
         .scaleLinear()
         .domain([0, gdpMax])
-        .range([svgHeight-margin.top-margin.bottom, 0]);
+        .range([height, 0]);
     xAxisScale = d3
         .scaleLinear()
         .domain([
             d3.min(gdpYears),
             d3.max(gdpYears)
         ])
-        .range([barPadding, svgWidth - barPadding - margin.left -margin.right]);
+        .range([
+            barPadding, width - barPadding
+        ]);
 
     xAxis = d3
         .axisBottom()
@@ -91,12 +86,11 @@ const loadData = async() => {
         .attr("id", "y-axis")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-    xAxisTranslate = svgHeight - margin.bottom;
-
+    const xAxisTranslate = svgHeight - margin.bottom;
     svg
         .append("g")
         .attr("id", "x-axis")
-        .attr("transform", `translate(${margin.left}, ${xAxisTranslate })`)
+        .attr("transform", `translate(${margin.left}, ${xAxisTranslate})`)
         .call(xAxis)
 
 }
@@ -112,9 +106,9 @@ loadData()
 // yScale(d[1]);     return `translate(${x}, ${y})`; }) .attr("width", barwidth
 // - barPadding) .attr("height", (d) => yScale(d[1])) .attr("fill", "grey")
 // function loadData() {     let req = new XMLHttpRequest();     req.onload =
-// function () {         if (this.readyState == 4 && this.status == 200) {
-//       let json = JSON.parse(this.responseText);             //
+// function () {         if (this.readyState == 4 && this.status == 200) { let
+// json = JSON.parse(this.responseText);             //
 // document.getElementById("demo").innerHTML = req.responseText;
 // console.log(json.data)         }     };     req.open("GET",
-// "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GD
-// P-data.json", true);     req.send(); }
+// "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/G
+// D P-data.json", true);     req.send(); }
